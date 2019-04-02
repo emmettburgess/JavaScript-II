152312c8511d2b4fc2f341a81a5ed659c42facaa
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-04-02 13:26:51 -0600

    answered callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -23,25 +23,39 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
+function hof(arr, com, cb) {
+  return cb(arr, com);
+}
 
-
-function getLength(arr, cb) {
+const getLength = function(arr, length) {
+  return arr.length;
   // getLength passes the length of the array into the callback.
 }
 
-function last(arr, cb) {
+const last = function(arr, last) {
+  return arr.pop();
   // last passes the last item of the array into the callback.
 }
 
-function sumNums(x, y, cb) {
+const sumNums = function(x, y, add) {
+  return x + y;
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
 
-function multiplyNums(x, y, cb) {
+const multiplyNums = function(x, y, mult) {
+  return x * y;
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
 
-function contains(item, list, cb) {
+const contains = function(arr, item, contains) {
+  for(i=0; i < arr.length; i++) {
+    if (i===item) {
+      return true;
+      else {
+        return false;
+      }
+    }
+  }
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
